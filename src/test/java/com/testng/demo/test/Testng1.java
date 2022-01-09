acbe7e2c728e8ceb7519577da9e1721fa903ac80
Venkatesh <viv7012@gmail.com>
2022-01-09 19:54:24 +0530

    write the first test case.

--- src/test/java/com/testng/demo/test/Testng1.java
+++ src/test/java/com/testng/demo/test/Testng1.java
@@ -1,5 +1,16 @@
 package com.testng.demo.test;
 
+import org.testng.annotations.*;
+
 public class Testng1 {
 
+	@Test
+	private void test1() {
+		System.out.println("First test case.");
+	}
+	
 }
+
+
+
+
