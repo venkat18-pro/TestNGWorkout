32c01de9c00a10d76c3abc90e8095205cde5ee25
Venkatesh <viv7012@gmail.com>
2022-01-09 19:57:18 +0530

    write the @AfterClass and @BeforeClass annotaion.

--- src/test/java/com/testng/demo/test/Testng1.java
+++ src/test/java/com/testng/demo/test/Testng1.java
@@ -4,11 +4,20 @@ import org.testng.annotations.*;
 
 public class Testng1 {
 
+	@BeforeClass
+	private void start() {
+		System.out.println("Start");
+	}
+	
 	@Test
 	private void test1() {
 		System.out.println("First test case.");
 	}
 	
+	@AfterClass
+	private void end() {
+		System.out.println("end");
+	}
 }
 
 
