2babbc02bda1772fac7faf359977ee4a56abb80e
Venkatesh <viv7012@gmail.com>
2022-01-10 10:02:17 +0530

    check the Cookie in the facebook web application.

--- src/test/java/com/testng/demo/test/Testng1.java
+++ src/test/java/com/testng/demo/test/Testng1.java
@@ -1,7 +1,9 @@
 package com.testng.demo.test;
 
 import java.util.Date;
+import java.util.Set;
 
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.html5.LocalStorage;
 import org.openqa.selenium.html5.SessionStorage;
@@ -33,8 +35,21 @@ public class Testng1 {
 		SessionStorage sl = driver.getSessionStorage();
 		System.out.println("size: "+sl.size());
 		System.out.println(sl.getItem("sp_pi"));
+		
 	}
 	
+	@Test
+	private void cookieTest() {
+
+		Cookie cookieNamed = driver.manage().getCookieNamed("fr");
+		System.out.println("Cookie Name: "+cookieNamed.getDomain());
+		
+		Set<Cookie> cookies = driver.manage().getCookies();
+		for (Cookie cookie : cookies) {
+			System.out.println("Name : "+cookie.getName());
+			System.out.println("Domain: "+cookie.getDomain());
+		}
+	}
 	@AfterMethod
 	private void endTime() {
 		System.out.println(new Date());
