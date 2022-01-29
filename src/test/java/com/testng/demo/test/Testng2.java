b23eaacf76a17be257be8e913d61d8a2da316012
Venkatesh <viv7012@gmail.com>
2022-01-29 20:25:40 +0530

    groups into REG and SAN.

--- src/test/java/com/testng/demo/test/Testng2.java
+++ src/test/java/com/testng/demo/test/Testng2.java
@@ -1,39 +1,46 @@
 package com.testng.demo.test;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Testng2 {
-
+	WebDriver driver;
+	
+	
 	@Test(groups= {"REG"})
 	private void test1() {
-		WebDriverManager.firefoxdriver().setup();
-		WebDriver driver = new FirefoxDriver();
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 		System.out.println("Test1 method REG");
+		driver.quit();
 	}
 	@Test(groups= {"SAN"})
 	private void test2() {
-		WebDriverManager.firefoxdriver().setup();
-		WebDriver driver = new FirefoxDriver();
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 		System.out.println("test2 method SAN");
+		driver.quit();
 	}	
 	@Test(groups="REG")
 	private void test3() {
-		WebDriverManager.firefoxdriver().setup();
-		WebDriver driver = new FirefoxDriver();
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 		System.out.println("test3 method REG");
+		driver.quit();
 	}	
 	@Test(groups="SAN")
 	private void test4() {
-		WebDriverManager.firefoxdriver().setup();
-		WebDriver driver = new FirefoxDriver();
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 		System.out.println("test4 method SAN");
+		driver.quit();
 	}
+	
 }
